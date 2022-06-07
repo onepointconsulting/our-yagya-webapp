72d19b366e227355f587ae90ace033b8614f7737
Gil Fernandes <gil.fernandes@onepointltd.com>
2022-06-07 17:41:08 +0100

    First steps with HTML and CSS.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import App from 'next/app'
+import { appWithTranslation } from 'next-i18next'
+import '../assets/css/styles.css'
+import { createContext } from 'react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export const GlobalContext = createContext({})
+
+/**
+ * Main entry point for the whole app.
+ * @param Component
+ * @param pageProps Contains global properties.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function OurYagya ({ Component, pageProps }) {
+    const { global } = pageProps
+    return (
+        <GlobalContext.Provider
+            value={global}><Component {...pageProps} />
+        </GlobalContext.Provider>
+    )
 }
 
-export default MyApp
+OurYagya.getInitialProps = async (ctxContainer) => {
+    // Calls page's `getInitialProps` and fills `appProps.pageProps`
+    const appProps = await App.getInitialProps(ctxContainer)
+    return { ...appProps, pageProps: { global: {} } }
+}
+
+export default appWithTranslation(OurYagya)
