45b839f348adb2299e408aa711be819e345adac7
Gil Fernandes <gil.fernandes@onepointltd.com>
2022-06-15 17:10:51 +0100

    First successful Apollo client integration.

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,17 @@ import LinksHome from '../components/LinksHome'
 import Recources from '../components/Resources'
 import Slider from '../components/Slider'
 import StickyMenu from '../components/StickyMenu'
+import { useEffect } from 'react'
+import { fetchFooters } from '../lib/apiClient'
 
 export default function Home() {
+
+    useEffect(() => {
+        fetchFooters()
+            .then(res => console.log('footer results', res))
+            .catch((e) => console.error("Failed to fetch footers", e))
+    }, [])
+
   return (
     <>
       <StickyMenu />
