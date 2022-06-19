16b50331dda728ec0e5e53c39ddca2c2300b4f15
murtazahassani105 <murtazahassani105@gmail.com>
2022-06-19 23:55:43 +0300

    added fetch data from the strapi in menus and links

--- components/header/PillMenu.jsx
+++ components/header/PillMenu.jsx
@@ -24,22 +24,22 @@ const PillMenu = () => {
  
     return (
       <div>
-          <div class={stickyPillMenu ? "flex sticky top-16 z-10 h-full" : "hidden"}>
-            <div class="flex flex-col float-right ml-auto mt-1" style={{ marginRight: '6px' }}>
-                <div class="p-2 m-1 px-2 text-yagya-red-color bg-white border-2 rounded-full border-yagya-red-color">
-                    <img class="h-5 w-5" src="../img/icons/Language.png" alt="" />
+          <div className={stickyPillMenu ? "flex sticky top-16 z-10 h-full" : "hidden"}>
+            <div className="flex flex-col float-right ml-auto mt-1" style={{ marginRight: '6px' }}>
+                <div className="p-2 m-1 px-2 text-yagya-red-color bg-white border-2 rounded-full border-yagya-red-color">
+                    <img className="h-5 w-5" src="../img/icons/Language.png" alt="" />
                 </div>
-                <div class="p-2 m-1 px-2 text-yagya-red-color bg-white border-2 rounded-full border-yagya-red-color">
-                    <a href="/login.html"> <img class="h-5 w-5" src="../img/icons/LoginRed.png" alt="" />
+                <div className="p-2 m-1 px-2 text-yagya-red-color bg-white border-2 rounded-full border-yagya-red-color">
+                    <a href="/login.html"> <img className="h-5 w-5" src="../img/icons/LoginRed.png" alt="" />
                     </a>
                 </div>
-                <div class="p-2 m-1 px-2 text-yagya-red-color bg-white border-2 rounded-full border-yagya-red-color">
+                <div className="p-2 m-1 px-2 text-yagya-red-color bg-white border-2 rounded-full border-yagya-red-color">
 
-                    <a href="/made4me.html"> <img class="h-5 w-5" src="../img/icons/StarPointer.png" alt="" />
+                    <a href="/made4me.html"> <img className="h-5 w-5" src="../img/icons/StarPointer.png" alt="" />
                     </a>
                 </div>
-                <div class="p-2 m-1 px-2 text-yagya-red-color bg-white border-2 rounded-full border-yagya-red-color">
-                    <img class="h-5 w-5" src="../img/icons/HeartRed.png" alt="" />
+                <div className="p-2 m-1 px-2 text-yagya-red-color bg-white border-2 rounded-full border-yagya-red-color">
+                    <img className="h-5 w-5" src="../img/icons/HeartRed.png" alt="" />
                 </div>
             </div>
         </div>
