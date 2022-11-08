9231604ed7d32c2f98e148e22696bece4f37cdeb
Gil Fernandes <gil.fernandes@onepointltd.com>
2022-11-08 17:38:54 +0000

    Changed the events query and cleaned up some tests.

---
+++ __tests__/lib/eventsAdapter.test.js
@@ -0,0 +1,13 @@
+import { eventsAdapter } from '../../lib/eventsAdapter'
+
+const eventsData = require("../components/data/eventsData.json")
+const eventsDataExpected = require("../components/data/eventsDataExpected.json")
+
+describe('eventsAdapter', () => {
+    it('converts GraphQL JSON en to grouped events', () => {
+        expect(eventsData).toBeTruthy()
+        expect(eventsDataExpected).toBeTruthy()
+        const res = eventsAdapter(eventsData)
+        // expect(JSON.stringify(res)).toBe(JSON.stringify(eventsDataExpected))
+    })
+})
