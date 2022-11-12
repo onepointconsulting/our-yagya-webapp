ba3d806777c2f831b337c2afdacdefb6a3cb4d27
murtazahassani69 <murtazahassani105@gmail.com>
2022-11-12 13:47:53 +0200

    fixed the html layout a bit, cleaned up the data in strapi

--- components/Events/EventsMain.jsx
+++ components/Events/EventsMain.jsx
@@ -1,38 +1,44 @@
-import React, { useContext } from 'react'
-import { eventsAdapter } from '../../lib/eventsAdapter'
-import FeaturedEventBlock from './FeaturedEventBlock'
-import { GlobalContext } from '../../pages/_app'
-import { categoriesAdapter } from '../../lib/globalPropertiesAdapter'
-import { imageAdapter } from '../../lib/imageAdapter'
-import EventBlocks from './EventBlocks'
+import React, { useContext } from "react";
+import { eventsAdapter } from "../../lib/eventsAdapter";
+import FeaturedEventBlock from "./FeaturedEventBlock";
+import { GlobalContext } from "../../pages/_app";
+import { categoriesAdapter } from "../../lib/globalPropertiesAdapter";
+import { imageAdapter } from "../../lib/imageAdapter";
+import EventBlocks from "./EventBlocks";
 
-export default function EventsMain ({ data }) {
-    const { globalProperties } = useContext(GlobalContext)
+export default function EventsMain({ data }) {
+  const { globalProperties } = useContext(GlobalContext);
+  const bgImage = imageAdapter(data);
 
-    console.log('EventsMain', JSON.stringify(data))
+  
+  const eventsCategories = categoriesAdapter(globalProperties,"eventsCategories" );
+      const adaptedEvents = eventsAdapter(data);
 
-    const eventsCategories = categoriesAdapter(globalProperties,
-        'eventsCategories', '::')
-    const adaptedEvents = eventsAdapter(data)
-    const bgImage = imageAdapter(data)
-
-    return (
-        <div>
-            <div className="flex flex-col items-center w-full">
-                <div
-                    className="flex justify-center w-full h-auto mb-auto bg-opacity-75 imageBack"
-                    style={{
-                        background: `url(${bgImage}) center center / cover no-repeat`,
-                    }}
-                >
-                    <FeaturedEventBlock adaptedEvents={adaptedEvents}
-                                        title={eventsCategories[0]}/>
-                    {' '}
-                    <EventBlocks adaptedEvents={adaptedEvents}
-                                 eventsCategories={eventsCategories} />
-
-                </div>
-            </div>
+     console.log("eventsCategories",adaptedEvents)
+  return (
+    <>
+      <div
+        className="flex justify-center w-full h-auto mb-auto bg-opacity-75 imageBack"
+        style={{
+          background: `url(${bgImage}) center center / cover no-repeat`,
+        }}
+      >
+           {/* Featured event with the event background */}
+        <div className="w-full mx-4">
+          <FeaturedEventBlock
+            adaptedEvents={adaptedEvents}
+            title={eventsCategories[0]}
+          />
         </div>
-    )
+      </div>
+
+      {/* events */}
+      <div className="w-full">
+        <EventBlocks
+          adaptedEvents={adaptedEvents}
+          eventsCategories={eventsCategories}
+        />
+      </div>
+    </>
+  );
 }
