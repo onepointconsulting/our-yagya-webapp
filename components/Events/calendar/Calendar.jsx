e7e3d31ecf3aac1fc1462ca3a1df2f0d425d4b0a
Gil Fernandes <gil.fernandes@onepointltd.com>
2022-12-01 20:35:18 +0000

    Got the calendar working.

--- components/Events/calendar/Calendar.jsx
+++ components/Events/calendar/Calendar.jsx
@@ -1,11 +1,18 @@
-import FullCalendar from '@fullcalendar/react'
-import { dayGridPlugin, interactionPlugin } from '@fullcalendar/interaction'
+import FullCalendar from "@fullcalendar/react";
+import interactionPlugin from "@fullcalendar/interaction";
+import dayGridPlugin from '@fullcalendar/daygrid'
+import { useRef } from "react";
 
 const Calendar = () => {
-  return <FullCalendar
-    plugins={[ dayGridPlugin ]}
-    initialView="dayGridMonth"
-  />
+  const calendarRef = useRef(null);
+  return (
+    <FullCalendar
+      innerRef={calendarRef}
+      plugins={[dayGridPlugin, interactionPlugin]}
+      editable
+      selectable
+    />
+  );
 }
 
 export default Calendar
