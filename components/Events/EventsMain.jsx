ab6972e339a5951360801c8f83298fd3ba1f2b9b
Gil Fernandes <gil.fernandes@onepointltd.com>
2022-12-09 17:19:10 +0000

    Some formatting.

--- components/Events/EventsMain.jsx
+++ components/Events/EventsMain.jsx
@@ -5,43 +5,40 @@ import { categoriesAdapter } from '../../lib/globalPropertiesAdapter'
 import { imageAdapter } from '../../lib/imageAdapter'
 import EventBlocks from './EventBlocks'
 import EventBlock from './EventBlock'
+import { processEventDates } from '../../lib/dateAdapter'
 
 export default function EventsMain ({ data }) {
-    const { globalProperties } = useContext(GlobalContext)
-    const bgImage = imageAdapter(data)
+  const { globalProperties } = useContext(GlobalContext)
+  const bgImage = imageAdapter(data)
 
-    const eventsCategories = categoriesAdapter(globalProperties,
-        'eventsCategories')
-    const adaptedEvents = eventsAdapter(data)
+  const eventsCategories = categoriesAdapter(globalProperties,
+    'eventsCategories')
+  const adaptedEvents = eventsAdapter(data)
 
-    return (
-        <>
-            <div
-                className="flex justify-center w-full h-auto mb-auto bg-opacity-75 imageBack"
-                style={{
-                    background: `url(${bgImage}) center center / cover no-repeat`,
-                }}
-            >
-                {/* Featured event with the event background */}
-                <div className="w-full mx-2 md:mx-4 lg:mx-24 pt-96">
-                    {/* <FeaturedEventBlock
-                        adaptedEvents={adaptedEvents}
-                        title={eventsCategories[0]}
-                    /> */}
-                    <EventBlock
-                    adaptedEvents={adaptedEvents}
-                    category={"Featured Events"}
-                />
-                </div>
-            </div>
+  return (
+    <>
+      <div
+        className="flex justify-center w-full h-auto mb-auto bg-opacity-75 imageBack"
+        style={{
+          background: `url(${bgImage}) center center / cover no-repeat`,
+        }}
+      >
+        {/* Featured event with the event background */}
+        <div className="w-full mx-2 md:mx-4 lg:mx-24 pt-96">
+          <EventBlock
+            adaptedEvents={adaptedEvents}
+            category={'Featured Events'}
+          />
+        </div>
+      </div>
 
-            {/* events */}
-            <div className="w-full mt-24">
-                <EventBlocks
-                    adaptedEvents={adaptedEvents}
-                    eventsCategories={eventsCategories}
-                />
-            </div>
-        </>
-    )
+      {/* events */}
+      <div className="w-full mt-24">
+        <EventBlocks
+          adaptedEvents={adaptedEvents}
+          eventsCategories={eventsCategories}
+        />
+      </div>
+    </>
+  )
 }
