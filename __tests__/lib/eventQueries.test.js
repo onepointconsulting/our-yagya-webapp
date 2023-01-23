1f2323d6ed5a392a32042db7e2533bdce772db9a
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-01-23 12:10:02 +0000

    Added method to generate the query for the event slider.

---
+++ __tests__/lib/eventQueries.test.js
@@ -0,0 +1,9 @@
+import { querySliderEvents } from '../../lib/eventQueries'
+
+describe('eventQueries', () => {
+  it('creates query for slider events', () => {
+    const query = querySliderEvents('en', 5)
+    expect(query).toBeTruthy()
+    expect(query.includes("locale: \"en\"")).toBe(true)
+  })
+})
