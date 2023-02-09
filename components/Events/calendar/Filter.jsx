64e3441ab4ba3bd9294425b92d20945536709db3
Sotirios Angelopoulos <sotiris.angelopoulos@gmail.com>
2023-02-09 10:52:23 +0200

    Calendar: set private-event flag in the state

--- components/Events/calendar/Filter.jsx
+++ components/Events/calendar/Filter.jsx
@@ -7,7 +7,7 @@ import findStartAndEndMonth from "../../../lib/dateAdapter";
 const Filter = () => {
     
   const { calendarData, dispatchCalendarData } = useContext(CalendarContext);
-  const { online, inHouse, eventType, language, eventTypes, languages, venue, venues, categoryId } = calendarData;
+  const { online, inHouse, privateEvent, eventType, language, eventTypes, languages, venue, venues, categoryId } = calendarData;
   const { start, end } = findStartAndEndMonth()
 
   const onEventTypeChange = (event) => {
@@ -18,17 +18,22 @@ const Filter = () => {
     dispatchCalendarData({ type: CALENDAR_ACTIONS.SET_VENUE, venue: event.target.value})
   } 
 
+  const onPrivacyChange = (event) => {
+    dispatchCalendarData({ type: CALENDAR_ACTIONS.SET_PRIVATE, privateEvent: event.target.value})
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       const eventTypeParam = eventType !== 'ALL' ? `&eventType=${eventType}` : ''
       const languageParam = language  ? `&language=${language}` : '' // TODO: language is currently fixed w/ categoryId
       const venueParam = venue !== 'ALL' ? `&venueLocality=${venue}` : ''
       const onlineStatusParam = online && inHouse ? '' : online ? `&onlineStatus=online` : `&onlineStatus=inhouse`
-      const result = await axios.get(`${getServerURL()}/api/events/category-events/${categoryId}?limit=5&startDateTime=${start}T00:00:00.000Z&endDateTime=${end}T00:00:00.000Z${eventTypeParam}${languageParam}${venueParam}${onlineStatusParam}`)
+      const privateEventParam = privateEvent === "true" ? `&privateEvent=true` : ""
+      const result = await axios.get(`${getServerURL()}/api/events/category-events/${categoryId}?limit=5&startDateTime=${start}T00:00:00.000Z&endDateTime=${end}T00:00:00.000Z${eventTypeParam}${languageParam}${venueParam}${onlineStatusParam}${privateEventParam}`)
       dispatchCalendarData({type: CALENDAR_ACTIONS.SET_EVENTS, events: result.data.events});
     };
     if (categoryId) fetchData();
-  }, [online, inHouse, eventType, language, venue]);
+  }, [online, inHouse, eventType, language, venue, privateEvent]);
 
   return (
     <div className="mt-12">
@@ -85,9 +90,9 @@ const Filter = () => {
             </select>
           </div>
           <div className="mx-4 my-4 relative">
-            <select className="filter appearance-none">
-              <option>suitable for BKS</option>
-              <option>Spanish</option>
+            <select className="filter appearance-none" value={privateEvent} onChange={onPrivacyChange}>
+              <option value="false">All</option>
+              <option value="true">Private</option>
             </select>
           </div>
         </div>
--- context/CalendarContext.js
+++ context/CalendarContext.js
@@ -14,17 +14,17 @@ export const CALENDAR_ACTIONS = {
   SET_LANGUAGE: 'SET_LANGUAGE',
   SET_VENUES: 'SET_VENUES',
   SET_VENUE: 'SET_VENUE',
-  SET_SUITABLES: 'SET_SUITABLES',
-  SET_SUITABLE: 'SET_SUITABLE',
+  // SET_SUITABLES: 'SET_SUITABLES',
+  // SET_SUITABLE: 'SET_SUITABLE',
   TOGGLE_ONLINE: 'TOGGLE_ONLINE',
   TOGGLE_IN_HOUSE: 'TOGGLE_IN_HOUSE',
   SET_EVENTS: 'SET_EVENTS',
   SET_CATEGORY_ID: 'SET_CATEGORY_ID',
+  SET_PRIVATE: 'SET_PRIVATE'
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
-    // SET_EVENT LIST , SET VENUE LIST , SET etc etc 
     case CALENDAR_ACTIONS.SET_EVENT_TYPE:
       return {
         ...state,
@@ -59,19 +59,21 @@ const reducer = (state, action) => {
       return {
         ...state,
         online: !!state.inHouse ? !state.online : state.online
-        // online: !state.online
       }
     case CALENDAR_ACTIONS.TOGGLE_IN_HOUSE:
       return {
         ...state,
         inHouse: !!state.online ? !state.inHouse : state.inHouse
-        // inHouse: !state.inHouse
+      }
+    case CALENDAR_ACTIONS.SET_PRIVATE:
+      return {
+        ...state,
+        privateEvent: action.privateEvent
       }
     case CALENDAR_ACTIONS.SET_EVENTS:
       return {
         ...state,
         events: action.events
-      
     }
     case CALENDAR_ACTIONS.SET_CATEGORY_ID:
       return {
@@ -91,9 +93,9 @@ export const CalendarContextProvider = props => {
       languages: [],
       eventTypes: [],
       venues: [],
-      suitables: [],
       online: true,
       inHouse: true,
+      privateEvent: false,
       eventType: DEFAULT_VALUES.EVENT_TYPE,
       venue: DEFAULT_VALUES.VENUE,
       events: [],
