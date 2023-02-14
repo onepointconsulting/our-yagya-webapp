2a347cba4bd59aabccfcaaf5529729265b8eb9b6
Murtaza Hassani <murtazahassani105@gmail.com>
2023-02-14 09:08:01 +0200

    fixed event calendar page, and event accordion

--- components/Events/calendar/EventsAccordion.jsx
+++ components/Events/calendar/EventsAccordion.jsx
@@ -12,15 +12,15 @@ const ButtonsAccordion = ({ event }) => {
   const { t } = useTranslation()
 
   return (
-    <div className="w-32 md:w-36 lg:w-72">
+    <div className="w-32 md:w-40 lg:w-60">
       <MoreInfo event={event}
-                mainClass="flex rounded-md bg-gold1 my-1 xl:my-2 px-1 xl:px-2 py-1 xl:py-2 cursor-pointer events-center"
+                mainClass="flex rounded-md bg-gold1 my-1 xl:my-2 px-1 xl:px-2 py-1 xl:py-2 cursor-pointer items-center"
                 imgClass="w-4 xs:w-6 lg:w-12"
-                moreInfoClass="md:pl-3 lg:pl-4 text-[9px] md:text-lg lg:text-xl xl:text-2xl text-white"/>
+                moreInfoClass="pl-1 md:pl-3 lg:pl-4 text-[9px] md:text-lg lg:text-xl xl:text-2xl text-white"/>
 
       {/* Register link button */}
       <div
-        className="flex rounded-md bg-gold1 events-center text-center my-1 xl:my-2 px-1 xl:px-2 py-1 xl:py-2 cursor-pointer">
+        className="flex items-center px-1 py-1 my-1 rounded-md cursor-pointer bg-gold1 xl:my-2 xl:px-2 xl:py-2">
         <div>
           <img
             className="w-4 xs:w-6 lg:w-12"
@@ -30,7 +30,7 @@ const ButtonsAccordion = ({ event }) => {
         </div>
         <Link href="#" target="_blank">
           <div
-            className="md:pl-3 lg:pl-4 text-[9px] md:text-lg lg:text-xl xl:text-2xl text-white">
+            className="pl-1 md:pl-3 lg:pl-4 text-[9px] md:text-lg lg:text-xl xl:text-2xl text-white">
             {t('Register')}
           </div>
         </Link>
@@ -58,35 +58,38 @@ export default function EventsAccordion () {
       {events.map((event, index) => (
         <div
           key={index}
-          className="relative bg-white my-4 rounded-lg shadow-lg"
+          className="relative my-4 bg-white rounded-lg shadow-lg"
         >
           {/* Accordion header */}
           <div
-            onClick={() => handleAccordionClick(index)}
-            className="flex justify-between bg-gray-100 cursor-pointer md:events-center metropolis_medium pr-4"
+            className="flex items-center justify-between pr-1 bg-gray-100 cursor-pointer md:pr-4 metropolis_medium"
           >
-            <div
-              className="flex events-center w-[93%] md:w-4/5 lg:w-5/6 xl:w-5/6">
-              <div className="relative w-[30vw] md:w-96">
+            <div  className="flex items-center w-[93%] md:w-4/5 lg:w-5/6 xl:w-5/6"
+                        onClick={() => handleAccordionClick(index)}
+            >
+              {/* event img */}
+              <div className="relative w-20 h-16 xss:w-32 md:w-44 lg:w-96 xss:h-28 md:h-32 lg:h-48">
                 <img
                   className="object-cover w-full h-full"
                   src={event.imageUrl}
                   alt={event.imageUrl}
                 />
               </div>
-              <div className="px-2 md:px-4 w-[9rem] md:w-[inherit] lg:w-full">
+              
+              {/* event title and date */}
+              <div className="px-2 md:px-4 w-36 xss:w-40 md:w-[23rem] lg:w-full">
                 <h1
-                  className="text-xs font-medium leading-4 text-redfull xs:text-[14px] md:text-xl xxl:text-3xl whitespace-nowrap lg:whitespace-normal overflow-hidden lg:overflow-auto text-ellipsis">
+                  className="text-xs font-medium leading-4 text-redfull xs:text-[14px] md:text-xl xl:text-3xl whitespace-nowrap lg:whitespace-normal overflow-hidden lg:overflow-auto text-ellipsis">
                   {event.title}
                 </h1>
                 {!!event.subTitle && <h1
-                  className="text-xs font-medium leading-4 text-redfull xs:text-[14px] md:text-lg xl:text-2xl whitespace-nowrap lg:whitespace-normal overflow-hidden lg:overflow-auto text-ellipsis">
+                  className="text-xs font-medium leading-4 text-redfull xs:text-[14px] md:text-lg xl:text-xl whitespace-nowrap lg:whitespace-normal overflow-hidden lg:overflow-auto text-ellipsis">
                   [{event.subTitle}]
                 </h1>}
-                <div className="md:flex events-center justify-between w-full">
+                <div className="items-center justify-between w-full pt-2 md:flex">
                   <div>
                     <p
-                      className="leading-6 text-[10px] text-gray-700 md:text-[1rem] lg:text-[1.125rem]">
+                      className="text-[8px] text-gray-400 md:text-sm lg:text-[1.125rem]">
                       {formateDate(
                         event.startDateTime,
                         locale,
@@ -96,7 +99,7 @@ export default function EventsAccordion () {
                   </div>
 
                   <div
-                    className="leading-6 text-[10px] text-gray-700 md:text-[1rem] lg:text-[1.125rem]">
+                    className="text-[8px] text-gray-400 md:text-sm lg:text-[1.125rem]">
                     <p>{formateDate(
                       event.startDateTime,
                       locale,
@@ -113,29 +116,29 @@ export default function EventsAccordion () {
           {/* Accordion body */}
           {activeIndex === index && (
             <div
-              className="relative  overflow-hidden transition duration-500 ease-in-out w-full text-white bg-white shadow-md metropolis_medium"
+              className="relative w-full overflow-hidden text-white transition duration-500 ease-in-out bg-white border-b border-l border-r metropolis_medium"
               style={{ height: activeIndex === index ? 'auto' : '0' }}
             >
               <div
                 className="flex flex-col-reverse justify-between p-3 lg:flex-row xl:p-5">
                 <div className="w-full">
                   <p
-                    className="text-gray-700 text-sm md:text-base lg:text-lg xl:text-xl"
+                    className="text-sm text-gray-400 md:text-base lg:text-lg xl:text-xl"
                     dangerouslySetInnerHTML={{ __html: event.description }}>
                   </p>
                 </div>
 
                 {/* <!-- img --> */}
                 <div
-                  className="relative flex justify-between events-end flex-col w-full lg:w-max">
+                  className="relative flex flex-col items-end justify-between w-full lg:w-max">
                   <div className="md:w-full lg:w-96 xl:w-10/12">
                     <img
-                      className="object-cover w-auto mx-auto lg:h-auto w-full"
+                      className="object-cover w-full mx-auto lg:h-auto"
                       src={event.imageUrl}
                       alt=""
                     />
                   </div>
-                  <div className="w-10 text-center relative">
+                  <div className="relative w-10 my-2 text-center">
                     <SocialMediaIcons eventIndex={event}/>
                   </div>
                   {' '}
