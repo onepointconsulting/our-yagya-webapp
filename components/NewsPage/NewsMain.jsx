20e13bc49c87af52995b2b45a36cabca515a8eed
Sotirios Angelopoulos <sotiris.angelopoulos@gmail.com>
2023-02-17 20:53:16 +0200

    News page, part B

--- components/NewsPage/NewsMain.jsx
+++ components/NewsPage/NewsMain.jsx
@@ -5,7 +5,6 @@ import { getNewsByCategory } from "../../lib/newsAdapter";
 import { GlobalContext } from "../../pages/_app";
 import { useTranslation } from "react-i18next";
 import Link from 'next/link'
-import { getFeaturedCategory } from "../../lib/eventsAdapter";
 
 const NewsItem = ({ news, locale }) => {
   const { t } = useTranslation();
@@ -26,60 +25,19 @@ const NewsItem = ({ news, locale }) => {
   );
 };
 
-const AccordionItem = ({
-  pos,
-  openPosition,
-  setOpenPosition,
-  subCategory,
-  locale,
-}) => {
-  const { t } = useTranslation();
-  const openBody = "42rem";
-  let expanded = pos === openPosition;
-  const maxHeightChildBody = expanded ? openBody : null;
-  const title = Object.keys(subCategory)[0];
-  const news = Object.values(subCategory)[0]?.news;
-
+const NewsBlock = ({title, news, locale}) => {
   return (
-    <div className="w-full my-2 md:m-2 md:ml-auto">
-      <div
-        className="flex items-center h-12 py-1 mx-auto border-2 cursor-pointer accordionChildHeader xs:py-4 md:py-8 md:mx-0 text-gold1 border-gold1"
-        onClick={() => setOpenPosition(expanded ? -1 : pos)}
-      >
-        <div className="mb-0 cursor-pointer text-gold1">
-          <h1 className="py-2 pl-4 pr-2 text-xl capitalize filosofia_italic bg-button-color md:text-4xl text-gold1">
-            {title}
-          </h1>
+    <>
+    {news?.length > 0 && 
+      <div className="relative w-full my-4 bg-gray-200 h-fit">
+        <div className="py-2 pl-4 pr-2 text-xl capitalize text-white filosofia_italic bg-button-color bg-gold1 md:text-4xl">
+          {title}
         </div>
-      </div>
-
-      <div
-        className="overflow-hidden bg-gray-200 accordionChildBody"
-        style={{ maxHeight: maxHeightChildBody }}
-      >
-        {news.map((regionalNews, i) => {
-          return (
-            <div key={i} className="p-2 pl-3 metropolis_medium md:pl-6 md:p-3">
-              <div className="text-redfull py-2 text-[19px] md:text-[26px] xl:text-[30px]">
-              
-              <Link href={`/single_news/${regionalNews.id}`}>
-                {regionalNews.title}
-              </Link>
-              </div>
-              <div>
-                <div className="flex items-center text-sm text-gray-700 md:text-xl xl:text-2xl">
-                  <div className="mr-2">{t("Posted")}</div>
-                  <div className="mx-2">
-                    {formateDate(regionalNews.timestamp, locale)}
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-
-        <div className="flex justify-center mx-auto my-8">
-          <h1 className="flex justify-center text-5xl text-center text-gray-400 cursor-poainter">
+        {news?.map((news, i) => (
+          <NewsItem news={news} locale={locale} key={`${title}_${i}`} />
+        ))}
+        <div className="my-8">
+          <h1 className="flex justify-center mt-4 text-5xl text-center text-gray-400 cursor-poainter">
             <img
               className="w-8 md:w-12"
               src="/img/icons/ArrowDown.png"
@@ -88,10 +46,77 @@ const AccordionItem = ({
             />
           </h1>
         </div>
-      </div>
-    </div>
-  );
-};
+      </div>}
+      </>
+  )
+}
+
+// const AccordionItem = ({
+//   pos,
+//   openPosition,
+//   setOpenPosition,
+//   subCategory,
+//   locale,
+// }) => {
+//   const { t } = useTranslation();
+//   const openBody = "42rem";
+//   let expanded = pos === openPosition;
+//   const maxHeightChildBody = expanded ? openBody : null;
+//   const title = Object.keys(subCategory)[0];
+//   const news = Object.values(subCategory)[0]?.news;
+
+//   return (
+//     <div className="w-full my-2 md:m-2 md:ml-auto">
+//       <div
+//         className="flex items-center h-12 py-1 mx-auto border-2 cursor-pointer accordionChildHeader xs:py-4 md:py-8 md:mx-0 text-gold1 border-gold1"
+//         onClick={() => setOpenPosition(expanded ? -1 : pos)}
+//       >
+//         <div className="mb-0 cursor-pointer text-gold1">
+//           <h1 className="py-2 pl-4 pr-2 text-xl capitalize filosofia_italic bg-button-color md:text-4xl text-gold1">
+//             {title}
+//           </h1>
+//         </div>
+//       </div>
+
+//       <div
+//         className="overflow-hidden bg-gray-200 accordionChildBody"
+//         style={{ maxHeight: maxHeightChildBody }}
+//       >
+//         {news.map((regionalNews, i) => {
+//           return (
+//             <div key={i} className="p-2 pl-3 metropolis_medium md:pl-6 md:p-3">
+//               <div className="text-redfull py-2 text-[19px] md:text-[26px] xl:text-[30px]">
+              
+//               <Link href={`/single_news/${regionalNews.id}`}>
+//                 {regionalNews.title}
+//               </Link>
+//               </div>
+//               <div>
+//                 <div className="flex items-center text-sm text-gray-700 md:text-xl xl:text-2xl">
+//                   <div className="mr-2">{t("Posted")}</div>
+//                   <div className="mx-2">
+//                     {formateDate(regionalNews.timestamp, locale)}
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           );
+//         })}
+
+//         <div className="flex justify-center mx-auto my-8">
+//           <h1 className="flex justify-center text-5xl text-center text-gray-400 cursor-poainter">
+//             <img
+//               className="w-8 md:w-12"
+//               src="/img/icons/ArrowDown.png"
+//               alt={title}
+//               title={title}
+//             />
+//           </h1>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default function NewsMain({ data }) {
   const { locale } = useContext(GlobalContext);
@@ -101,18 +126,24 @@ export default function NewsMain({ data }) {
   const allNews = data.data.news;
   // const featuredCategory = getFeaturedCategory(allNews)  
   
-  // International
-  const internationalNews = getNewsByCategory("International", allNews)
-  // Madhuban News
-  const madhubanNews = getNewsByCategory("Madhuban News", allNews)
-  // Special Focus News
-  const specialFocusNews = getNewsByCategory("Special Focus News", allNews)
-  // Transitions
-  const transitionsNews = getNewsByCategory("Transitions", allNews)
-  // Regional News
-  const regionalNews = getNewsByCategory("Regional News", allNews)
+  const INTERNATIONAL_NEWS = "International"
+  const MADHUBAN_NEWS = "Madhuban News"
+  const SPECIAL_FOCUS_NEWS = "Special Focus News"
+  const TRANSITIONS = "Transitions"
+  const REGIONAL_NEWS = "Regional News"
 
-  console.log(newsCategories, internationalNews, madhubanNews, specialFocusNews)
+  // International
+  const internationalNews = getNewsByCategory(INTERNATIONAL_NEWS, allNews)
+  // Madhuban News
+  const madhubanNews = getNewsByCategory(MADHUBAN_NEWS, allNews)
+  // Special Focus News
+  const specialFocusNews = getNewsByCategory(SPECIAL_FOCUS_NEWS, allNews)
+  // Transitions
+  const transitionsNews = getNewsByCategory(TRANSITIONS, allNews)
+  // Regional News
+  const regionalNews = getNewsByCategory(REGIONAL_NEWS, allNews)
+
+  // console.log(newsCategories, internationalNews, madhubanNews, specialFocusNews)
 
   return (
     <div>
@@ -128,10 +159,10 @@ export default function NewsMain({ data }) {
         <div className="w-auto mb-8 fadeInTop lg:w-full mx-atuo">
         <div className="object-cover bg-gray-200 bg-opacity-[0.50]">
             <div className="py-2 pl-4 pr-2 text-xl capitalize text-white filosofia_italic bg-button-color bg-gold1 md:text-4xl">
-              {newsCategories[0].title}
+              {INTERNATIONAL_NEWS}
             </div>
             {internationalNews?.map((news, i) => (
-              <NewsItem news={news} locale={locale} key={`news_item_${i}`} />
+              <NewsItem news={news} locale={locale} key={`${INTERNATIONAL_NEWS}_${i}`} />
             ))}
 
             <div className="py-8">
@@ -139,8 +170,8 @@ export default function NewsMain({ data }) {
                 <img
                   className="w-8 md:w-12"
                   src="/img/icons/ArrowDown.png"
-                  alt={newsCategories[0].title}
-                  title={newsCategories[0].title}
+                  alt={INTERNATIONAL_NEWS}
+                  title={INTERNATIONAL_NEWS}
                 />
               </h1>
             </div>
@@ -149,72 +180,16 @@ export default function NewsMain({ data }) {
             )}
         <div className="justify-between lg:flex">
           {/* Madhuban News */}
-         { madhubanNews?.length > 0 && (
-          <div className="flex flex-col items-center w-full lg:mr-4 lg:w-1/2">
-            <div className="relative w-full my-4 bg-gray-200 h-fit">
-              <div className="py-2 pl-4 pr-2 text-xl capitalize text-white filosofia_italic bg-button-color bg-gold1 md:text-4xl">
-                {newsCategories[1].title}
-              </div>
-              {madhubanNews?.map((news, i) => (
-                <NewsItem news={news} locale={locale} key={`news_item_${i}`} />
-              ))}
-              <div className="my-8">
-                <h1 className="flex justify-center mt-4 text-5xl text-center text-gray-400 cursor-poainter">
-                  <img
-                    className="w-8 md:w-12"
-                    src="/img/icons/ArrowDown.png"
-                    alt={newsCategories[1].title}
-                    title={newsCategories[1].title}
-                  />
-                </h1>
-              </div>
-            </div>  
-          </div>)}
-
+          <NewsBlock title={MADHUBAN_NEWS} news={madhubanNews} locale={locale} />
+        
           {/* Regional Websites */}
           <div className="flex flex-col items-center w-full lg:ml-4 lg:w-3/6">
             
             {/* Special Focus News */}
-            {specialFocusNews?.length > 0 && (
-            <div className="relative w-full my-4 bg-gray-200 h-fit">
-              <div className="py-2 pl-4 pr-2 text-xl capitalize text-white filosofia_italic bg-button-color bg-gold1 md:text-4xl">
-                {newsCategories[2].title}
-              </div>
-              {specialFocusNews?.map((news, i) => (
-                <NewsItem news={news} locale={locale} key={`news_item_${i}`} />
-              ))}
-              <div className="my-8">
-                <h1 className="flex justify-center mt-4 text-5xl text-center text-gray-400 cursor-poainter">
-                  <img
-                    className="w-8 md:w-12"
-                    src="/img/icons/ArrowDown.png"
-                    alt={newsCategories[2].title}
-                    title={newsCategories[2].title}
-                  />
-                </h1>
-              </div>
-            </div>)}
+            <NewsBlock title={SPECIAL_FOCUS_NEWS} news={specialFocusNews} locale={locale} />
 
             {/* Transitions */}
-            {transitionsNews?.length > 0 && (
-            <div className="relative w-full my-4 bg-gray-200 h-fit">
-              <div className="py-2 pl-4 pr-2 text-xl capitalize text-white filosofia_italic bg-button-color bg-gold1 md:text-4xl">
-                {newsCategories[4].title}
-              </div>
-              {transitionsNews?.map((news, i) => (
-                <NewsItem news={news} locale={locale} key={`news_item_${i}`} />
-              ))}
-              <div className="my-8">
-                <h1 className="flex justify-center mt-4 text-5xl text-center text-gray-400 cursor-poainter">
-                  <img
-                    className="w-8 md:w-12"
-                    src="/img/icons/ArrowDown.png"
-                    alt={newsCategories[4].title}
-                    title={newsCategories[4].title}
-                  />
-                </h1>
-              </div>
-            </div>)}
+            <NewsBlock title={TRANSITIONS} news={transitionsNews} locale={locale} />
 
             {/* Links */}
             <div className="relative w-full my-4 bg-gray-200">
